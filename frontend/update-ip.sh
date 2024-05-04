b03dbd8d9c94afc8cf742d1f1ce4885578f1101e
Micsta5885 <149246671+Micsta5885@users.noreply.github.com>
2024-05-04 18:43:41 +0200

    Add files via upload

---
+++ frontend/update-ip.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+API_URL="http://169.254.169.254/latest/api"
+TOKEN=$(curl -X PUT "$API_URL/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 600")
+TOKEN_HEADER="X-aws-ec2-metadata-token: $TOKEN"
+METADATA_URL="http://169.254.169.254/latest/meta-data"
+IP_V4=$(curl -H "$TOKEN_HEADER" -s $METADATA_URL/public-ipv4)
+
+sed -i "s/http:\/\/[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\:3000/http:\/\/$IP_V4:3000/g" index.html
+
+echo "Updated IP address in index.html to $IP_V4"
