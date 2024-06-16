465960e85dc9d134752401ff029871670be41fa9
Micsta5885 <149246671+Micsta5885@users.noreply.github.com>
2024-06-16 12:51:36 +0200

    Update update-ip.sh

--- frontend/update-ip.sh
+++ frontend/update-ip.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
 API_URL="http://169.254.169.254/latest/meta-data"
-TOKEN=$(curl -X PUT "$API_URL/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 600")
-TOKEN_HEADER="X-aws-ec2-metadata-token: $TOKEN"
-METADATA_URL="http://169.254.169.254/latest/meta-data"
-IP_V4=$(curl -H "$TOKEN_HEADER" -s $METADATA_URL/public-ipv4)
+IP_V4=$(curl -s $API_URL/public-ipv4)
 
 sudo sed -i "s|http://__BACKEND_IP__:3000|http://$IP_V4:3000|g" /home/ubuntu/app/frontend/nginx.conf
 sudo sed -i "s|http://localhost:3000|http://$IP_V4:3000|g" /home/ubuntu/app/frontend/index.html
 
 echo "Updated IP address in index.html and nginx.conf to $IP_V4"
+
